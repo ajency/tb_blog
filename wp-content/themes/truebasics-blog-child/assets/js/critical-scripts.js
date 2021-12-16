87397007f7784a10933df74d19e67a10090b189f
Mamata <mamata@ajency.in>
2021-12-16 18:57:44 +0530

    Implemented sticky header

--- wp-content/themes/truebasics-blog-child/assets/js/critical-scripts.js
+++ wp-content/themes/truebasics-blog-child/assets/js/critical-scripts.js
@@ -35,3 +35,48 @@ $(document).ready(function(){
 		$(".hero-banner .banner-container .single-slide .single-slide__content2").css("width", $smallSlideWidth);
 	});
 }); 
+
+/* header scroll */
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header.site-header').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('header.site-header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('header.site-header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
+	
+	if(lastScrollTop == 0){
+		$("header.site-header .tb-topbar").slideDown();
+	}else{
+		$("header.site-header .tb-topbar").slideUp();
+	}
+}
